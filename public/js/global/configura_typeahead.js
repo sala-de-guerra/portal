8d90f225ff18ea09831d5a5b6339bcb8c074ed69
Carlos Alberto Dalcin David <c142765@corp.caixa.gov.br>
2019-11-08 16:21:34 -0300

    google funcionando pre-alpha

--- public/js/global/configura_typeahead.js
+++ public/js/global/configura_typeahead.js
@@ -30,7 +30,8 @@ $(document).ready(function(){
     'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
     'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
     'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming','Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming','Alabama', 
+    'Alaska', 'Arizona', 'Arkansas', 'California',
     'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
     'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
     'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
@@ -38,7 +39,8 @@ $(document).ready(function(){
     'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
     'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
     'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming','Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming','Alabama', 
+    'Alaska', 'Arizona', 'Arkansas', 'California',
     'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
     'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
     'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
@@ -46,7 +48,8 @@ $(document).ready(function(){
     'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
     'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
     'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming','Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming','Alabama', 
+    'Alaska', 'Arizona', 'Arkansas', 'California',
     'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
     'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
     'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
@@ -61,6 +64,29 @@ $(document).ready(function(){
   'Audi', 'BMW', 'Bugatti', 'Ferrari', 'Ford', 'Lamborghini', 'Mercedes Benz', 'Porsche', 'Rolls-Royce', 'Volkswagen',
   'Audi', 'BMW', 'Bugatti', 'Ferrari', 'Ford', 'Lamborghini', 'Mercedes Benz', 'Porsche', 'Rolls-Royce', 'Volkswagen'];
 
+  // var bens = [
+  //   {
+  //   "numeroBem": "1444406706576",
+  //   "endereco": "VIA DAS MAGNOLIAS",
+  //   "nomeProponente": "CAMSTROIAMIREEA PJANILA",
+  //   "cpfCnpjProponente": "35868830003",
+  //   "nomeExMutuario": "MAAMPOORO ELALEIRA RAMCHO DE CLIVS",
+  //   "cpfCnpjExMutuario": "26834378997",
+  //   "id": "1"
+  //   },
+  //   {
+  //   "numeroBem": "1444407050305",
+  //   "endereco": "AVENIDA ANTONIO MATHIAS DE CAMARGO",
+  //   "nomeProponente": "CRIACIPNS EOUREATIA DEDASZVAS",
+  //   "cpfCnpjProponente": "25858477081",
+  //   "nomeExMutuario": "JUOIELANTSTOE RADSSS",
+  //   "cpfCnpjExMutuario": "4529323493",
+  //   "id": "2"
+  //   }
+  // ];
+
+  // console.log(bens);
+
 
 // instantiate the bloodhound suggestion engine
   var engine = new Bloodhound({
@@ -74,17 +100,41 @@ $(document).ready(function(){
               //console.log("data", data.response.songs)
               return $.map(data.response.songs, function (song) {
                   return {
-                      title: song.title,
-                      artistName: song.artist_name
+                    title: song.title,
+                    artistName: song.artist_name
                   };
               });
           }
       }
   });
 
+  var imoveis = new Bloodhound({
+    datumTokenizer: function (datum) {
+        return Bloodhound.tokenizers.whitespace(datum.numeroBem);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+        url: "js/mock.json",
+        filter: function (data) {
+            // console.log(data.response.songs);
+            return $.map(data.response.bens, function (bem) {
+                return {
+                  numeroBem: bem.numero_bem,
+                  endereco: bem.endereco,
+                  nomeProponente: bem.nome_proponente,
+                  cpfCnpjProponente: bem.cpf_cnpj_proponente,
+                  nomeExMutuario: bem.nome_ex_mutuario,
+                  cpfCnpjExMutuario: bem.cpf_cnpj_ex_mutuario
+                };
+            });
+        }
+    }
+});
+
+
   // initialize the bloodhound suggestion engine
   engine.initialize();
-
+  imoveis.initialize();
 
   $('.typeahead').typeahead({
     hint: false,
@@ -119,10 +169,24 @@ $(document).ready(function(){
         ]
       }
     },
+    // {
+    //   name: 'bens',
+    //   // limit: 10,
+    //   source: substringMatcher(bens),
+    //   templates: {
+    //     header: '<h4 class="source-name">Imóvel</h4>',
+    //     empty: [
+    //       '<h4 class="source-name">Imóvel</h4>' +
+    //       '<div class="empty-message">' +
+    //         'Nenhum resultado encontrado.' +
+    //       '</div>'
+    //     ]
+    //   }
+    // },
     {
       name: 'engine',
       // limit: 10,
-      displayKey: 'title',
+      display: 'title',
       source: engine.ttAdapter(),
       templates: {
           header: '<h4 class="source-name">Songs</h4>',
@@ -134,7 +198,25 @@ $(document).ready(function(){
           ].join('\n'),
           suggestion: Handlebars.compile('<p>{{title}} by {{artistName}}</p>')
       }
+    },
+
+    {
+      name: 'imoveis',
+      // limit: 10,
+      display: 'numeroBem',
+      source: imoveis.ttAdapter(),
+      templates: {
+          header: '<h4 class="source-name">Imóveis</h4>',
+          empty: [
+            '<h4 class="source-name">Imóveis</h4>' +
+            '<div class="empty-message">' +
+              'Nenhum resultado encontrado.' +
+            '</div>'
+          ].join('\n'),
+          suggestion: Handlebars.compile('<p>CHB: {{numeroBem}} - Endereço: {{endereco}}</p>')
+      }
     }
+
   );
 
 
