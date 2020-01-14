70b963f55e0bb30a0038f1d4739da9194024b15e
Carlos Alberto Dalcin David <c142765@corp.caixa.gov.br>
2020-01-14 14:02:34 -0300

    altera selects do distrato para ordem alfabetica

--- public/js/global/formata_lista_distrato.js
+++ public/js/global/formata_lista_distrato.js
@@ -16,6 +16,18 @@ function _formataListaDistrato (numeroContrato, view) {
                                 '<p>' + item.idDistrato + '</p>' +
                             '</div>' +
                         '</div>' +
+                        '<div class="col-sm-3">' +
+                            '<div class="form-group">' +
+                                '<label>Modalidade de Compra::</label>' +
+                                '<p>' + item.modalidadeProposta + '</p>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="col-sm-6">' +
+                            '<div class="form-group">' +
+                                '<label>Observação:</label>' +
+                                '<p>' + item.observacaoDistrato + '</p>' +
+                            '</div>' +
+                        '</div>' +
                     '</div>' +
 
                     '<div class="row">' +
@@ -48,7 +60,7 @@ function _formataListaDistrato (numeroContrato, view) {
                     '<div class="row">' +    
                         '<div class="col-sm-3">' +
                             '<div class="form-group">' +
-                                '<label>Data de início:</label>' +
+                                '<label>Data de Início:</label>' +
                                 '<p class="formata-data-sem-hora">' + item.dataCadastro + '</p>' +
                             '</div>' +
                         '</div>' +
@@ -73,45 +85,43 @@ function _formataListaDistrato (numeroContrato, view) {
                     '</div>' +
 
                     '<div class="row">' +
-                        '<div class="col-sm-3">' +
-                            '<div class="form-group">' +
-                                '<label>Modalidade de compra::</label>' +
-                                '<p>' + item.modalidadeProposta + '</p>' +
-                            '</div>' +
-                        '</div>' +
-                        '<div class="col-sm-9">' +
-                            '<div class="form-group">' +
-                                '<label>Observação:</label>' +
-                                '<p>' + item.observacaoDistrato + '</p>' +
+                        '<div class="col-md-12">' +
+                            '<div class="card collapsed-card card-primary">' +
+                                '<div class="card-header cursor-pointer" data-card-widget="collapse">' +
+                                    '<h3 class="card-title">Despesas de Distrato Cadastradas</h3>' +
+                                    '<div class="card-tools">' +
+                                        '<button type="button" class="btn btn-tool"><i class="fas fa-plus"></i></button>' +
+                                    '</div>' +
+                                '</div>' +
+                                '<div class="card-body">' +
+                                    '<div class="row">' +    
+                                        '<div class="col-sm-12 table-responsive">' +
+                                            '<div class="form-group">' +
+                                                '<label>Despesas de Distrato Cadastradas:</label>' +
+                                                '<table id="tblDespesasDistrato' + item.idDistrato + '" class="table table-bordered table-striped dataTable">' +
+                                                    '<thead>' +
+                                                        '<tr>' +
+                                                            '<th>Tipo de Despesa</th>' +
+                                                            '<th>Valor</th>' +
+                                                            '<th>Data Efetiva</th>' +
+                                                        '</tr>' +
+                                                    '</thead>' +
+                                                    '<tbody>' +
+
+                                                    '</tbody>' +
+                                                '</table>' +
+                                            '</div>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
                             '</div>' +
                         '</div>' +
                     '</div>' +
 
-                    '<br>' +
-
                     '<div class="row">' +    
-                        '<div class="col-sm-12 table-responsive p-0">' +
-                            '<table id="tblDespesasDistrato' + item.idDistrato + '" class="table table-bordered table-striped">' +
-                                '<thead>' +
-                                    '<tr>' +
-                                        '<th>Tipo de Despesa</th>' +
-                                        '<th>Valor</th>' +
-                                        '<th>Data Efetiva</th>' +
-                                    '</tr>' +
-                                '</thead>' +
-                                '<tbody>' +
+                        '<div id="btnAlteraStatus' + item.idDistrato + '" class="col-sm-2"></div>' +
 
-                                '</tbody>' +
-                            '</table>' +
-                        '</div>' +
-                    '</div>' +
-
-                    '<br>' +
-
-                    '<div class="row">' +    
-                        '<div id="btnAlteraStatus' + item.idDistrato + '" class="col-sm-3"></div>' +
-
-                        '<div id="btnCadastraDespesaDistrato' + item.idDistrato + '" class="col-sm-3"></div>' +
+                        '<div id="btnCadastraDespesaDistrato' + item.idDistrato + '" class="col-sm-2"></div>' +
                     '</div>' +
 
                 '</li>' +
@@ -149,13 +159,13 @@ function _formataListaDistrato (numeroContrato, view) {
                                             '<select id="selectMotivo' + item.idDistrato + '" name="motivoDistrato" class="form-control" required>' +
                                                 '<option value="">Selecione</option>' +
                                                 '<option value="AÇÃO JUDICIAL">AÇÃO JUDICIAL</option>' +
-                                                '<option value="LEILÕES NEGATIVOS">LEILÕES NEGATIVOS</option>' +
-                                                '<option value="IMPOSSIBILIDADE DE REGISTRO DE AQUISIÇÃO">IMPOSSIBILIDADE DE REGISTRO DE AQUISIÇÃO</option>' +
-                                                '<option value="DESISTÊNCIA">DESISTÊNCIA</option>' +
                                                 '<option value="CRÉDITO NÃO APROVADO">CRÉDITO NÃO APROVADO</option>' +
-                                                '<option value="ERRO FORMAL DE EDITAL">ERRO FORMAL DE EDITAL</option>' +
-                                                '<option value="DIREITO DE PREFERÊNCIA DO EX-MUTUÁRIO">DIREITO DE PREFERÊNCIA DO EX-MUTUÁRIO</option>' +
+                                                '<option value="DESISTÊNCIA">DESISTÊNCIA</option>' +
                                                 '<option value="DISTRATO CANCELADO">DISTRATO CANCELADO</option>' +
+                                                '<option value="DIREITO DE PREFERÊNCIA DO EX-MUTUÁRIO">DIREITO DE PREFERÊNCIA DO EX-MUTUÁRIO</option>' +
+                                                '<option value="ERRO FORMAL DE EDITAL">ERRO FORMAL DE EDITAL</option>' +
+                                                '<option value="IMPOSSIBILIDADE DE REGISTRO DE AQUISIÇÃO">IMPOSSIBILIDADE DE REGISTRO DE AQUISIÇÃO</option>' +
+                                                '<option value="LEILÕES NEGATIVOS">LEILÕES NEGATIVOS</option>' +
                                             '</select>' +
                                         '</div>' +
 
@@ -163,19 +173,19 @@ function _formataListaDistrato (numeroContrato, view) {
                                             '<label>Alterar Status da Demanda:</label>' +
                                             '<select name="statusAnaliseDistrato" class="form-control" required>' +
                                                 '<option value="" selected>Selecione</option>' +
-                                                '<option value="INICIAR ANÁLISE">INICIAR ANÁLISE</option>' +
-                                                '<option value="EM ANÁLISE">EM ANÁLISE</option>' +
-                                                '<option value="AGUARDA DOC CLIENTE">AGUARDA DOC CLIENTE</option>' +
-                                                '<option value="AGUARDA COORDENADOR">AGUARDA COORDENADOR</option>' +
-                                                '<option value="AGUARDA GF">AGUARDA GF</option>' +
                                                 '<option value="AGUARDA AUT. EMGEA">AGUARDA AUT. EMGEA</option>' +
-                                                '<option value="PREPARANDO ORIENT. AG.">PREPARANDO ORIENT. AG.</option>' +
-                                                '<option value="ENCAMINHADO AGÊNCIA">ENCAMINHADO AGÊNCIA</option>' +
+                                                '<option value="AGUARDA COORDENADOR">AGUARDA COORDENADOR</option>' +
+                                                '<option value="AGUARDA DOC CLIENTE">AGUARDA DOC CLIENTE</option>' +
+                                                '<option value="AGUARDA GF">AGUARDA GF</option>' +
+                                                '<option value="AVERBAÇÃO DISTRATO">AVERBAÇÃO DISTRATO</option>' +
                                                 '<option value="COMANDOS NO CIWEB">COMANDOS NO CIWEB</option>' +
                                                 '<option value="COMANDOS NO SIMOV">COMANDOS NO SIMOV</option>' +
-                                                '<option value="AVERBAÇÃO DISTRATO">AVERBAÇÃO DISTRATO</option>' +
                                                 '<option value="CONCLUÍDO">CONCLUÍDO</option>' +
                                                 '<option value="CONSULTA JURIR">CONSULTA JURIR</option>' +
+                                                '<option value="EM ANÁLISE">EM ANÁLISE</option>' +
+                                                '<option value="ENCAMINHADO AGÊNCIA">ENCAMINHADO AGÊNCIA</option>' +
+                                                '<option value="INICIAR ANÁLISE">INICIAR ANÁLISE</option>' +
+                                                '<option value="PREPARANDO ORIENT. AG.">PREPARANDO ORIENT. AG.</option>' +
                                             '</select>' +
                                         '</div>' +
 
@@ -225,19 +235,20 @@ function _formataListaDistrato (numeroContrato, view) {
                                             '<label>Tipo de Despesa:</label>' +
                                             '<select name="tipoDespesaDistrato" class="form-control" required>' +
                                                 '<option value="">Selecione</option>' +
-                                                '<option value="despesaRecursosPróprios">Recursos Próprios</option>' +
-                                                '<option value="despesaMultaDistrato">Multa</option>' +
-                                                '<option value="despesaFinanciamentoDistrato">Financiamento</option>' +
-                                                '<option value="despesaFgtsDistrato">FGTS</option>' +
-                                                '<option value="despesaParcelamentoDistrato">Parcelamento</option>' +
-                                                '<option value="despesaItbiDistrato">ITBI</option>' +
-                                                '<option value="despesaCustasCartoráriasDistrato">Custas Cartorárias</option>' +
-                                                '<option value="despesaTaxasFinanciamentoDistrato">Taxas de Concessão de Financiamento</option>' +
-                                                '<option value="despesaCondominioDistrato">Condomínio</option>' +
-                                                '<option value="despesaIptuDistrato">IPTU</option>' +
-                                                '<option value="despesaBenfeitoriasDistrato">Benfeitorias</option>' +
                                                 '<option value="despesaAutorizadaReembolsoEmgea">Autorizadas Reembolso EMGEA</option>' +
+                                                '<option value="despesaBenfeitoriasDistrato">Benfeitorias</option>' +
+                                                '<option value="despesaComissaoLeiloeiroDistrato">Comissão de Leiloeiro</option>' +
+                                                '<option value="despesaCondominioDistrato">Condomínio</option>' +
+                                                '<option value="despesaCustasCartoráriasDistrato">Custas Cartorárias</option>' +
+                                                '<option value="despesaFgtsDistrato">FGTS</option>' +
+                                                '<option value="despesaFinanciamentoDistrato">Financiamento</option>' +
+                                                '<option value="despesaIptuDistrato">IPTU</option>' +
+                                                '<option value="despesaItbiDistrato">ITBI</option>' +
+                                                '<option value="despesaMultaDistrato">Multa</option>' +
                                                 '<option value="despesaOutrasDespesasDistrato">Outras Despesas</option>' +
+                                                '<option value="despesaParcelamentoDistrato">Parcelamento</option>' +
+                                                '<option value="despesaRecursosPróprios">Recursos Próprios</option>' +
+                                                '<option value="despesaTaxasFinanciamentoDistrato">Taxas de Concessão de Financiamento</option>' +
                                             '</select>' +
                                         '</div>' +
 
