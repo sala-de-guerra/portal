f1726cb14a1917fdf969a8e32eb3da0898e94439
cadd70 <cadd70@gmail.com>
2020-02-29 00:33:39 -0300

    altera logica js da view equipes / cria dinamicamente as listas de equipes recebendo json multinivel

--- public/js/portal/gerencial/equipes.js
+++ public/js/portal/gerencial/equipes.js
@@ -12,134 +12,114 @@ const Toast = Swal.mixin({
     timer: 3000
 });
 
-/*********************************************\
-| Função que cria um card para cada empregado |
-\*********************************************/
+/*********************************************************************\
+| Função que cria uma lista para cada equipe com empregados populados |
+\*********************************************************************/
 
 $(document).ready( function () {
 
-    $.getJSON('../js/empregados.json', function(dados) {
-
+    $.getJSON('../js/equipes.json', function(dados) {
+        // console.log(dados);
         $.each(dados, function(key, item) {
-
-            let calloutColor = '';
-
-            switch (item.nomeEquipe) {
-                case 'Celula Administrar':
-                    calloutColor = 'warning';
-                    break;
-                case 'Celula Gerencial':
-                    calloutColor = 'info';
-                    break;
-                case 'Celula Preparar':
-                    calloutColor = 'success';
-                    break;
-                case 'Celula Contratacao':
-                    calloutColor = 'danger';
-                    break;
-                default:
-                    calloutColor = 'info';
-            };
-
-            let card =
-                `<li id="` + item.matricula + `">` +
-                    `<div class="callout callout-` + calloutColor + ` row p-0">` +
-                        `<div class="col-md-3">` +
-                            `<img src="http://www.sr2576.sp.caixa/2017/foto.asp?matricula=` + item.matricula + `" class="img-circle elevation-2 user-image-resize-50px" alt="User Image" onerror="this.src='{{ asset('/img/question-mark.png') }}';">` +
-                        `</div>` +
-                        `<div class="col-md-9">` +
-                            `<h5 class="card-title">` + item.nomeCompleto + `</h5>` +
-                            `<p class="card-text"><small class="text-muted">` + item.nomeFuncao + `</small></p>` +
-                        `</div>` +
-                    `</div>` +
-                `</li>`
-            ;
             
-            switch (item.nomeEquipe) {
-                case 'Celula Administrar':
-                    $(card).appendTo('#listaCelulaAdministrar');
-                    break;
-                case 'Celula Gerencial':
-                    $(card).appendTo('#listaCelulaGerencia');
-                    break;
-                case 'Celula Preparar':
-                    $(card).appendTo('#listaCelulaPreparar');
-                    break;
-                case 'Celula Contratacao':
-                    $(card).appendTo('#listaCelulaContratacao');
-                    break;
-                default:
-                    $(card).appendTo('#listaCelulaGerencia');
-            }
+            let arrayEmpregados = [];
+
+            $.each(item.equipe, function(key, item) {
                 
-        });
-        
-    });
-    
-});
+                let card =
+                    `<li id="` + item.matricula + `">` +
+                        `<div class="callout callout-primary row p-0">` +
+                            `<div class="col-md-12" id="eventual` + item.matricula + `" style="display:none;">` +
+                                `<i class="fas fa-fw fa-crown px-1"></i>` +
+                            `</div>` +
+                            `<div class="col-md-3">` +
+                                `<img src="http://www.sr2576.sp.caixa/2017/foto.asp?matricula=` + item.matricula + `" class="img-circle elevation-2 user-image-resize-50px" alt="User Image" onerror="this.src='{{ asset('/img/question-mark.png') }}';">` +
+                            `</div>` +
+                            `<div class="col-md-9">` +
+                                `<h5 class="card-title">` + item.nomeCompleto + `</h5>` +
+                                `<p class="card-text"><small class="text-muted">` + item.nomeFuncao + `</small></p>` +
+                            `</div>` +
+                        `</div>` +
+                    `</li>`
+                ;
 
-/*****************************************************\
-| Função que troca a cor do toast se o POST der certo |
-\*****************************************************/
+                arrayEmpregados.push(card);
 
-function trocarCor(celula, callout) {
-    $(callout).removeClass('callout-warning callout-info callout-success callout-danger');
-
-    switch (celula) {
-        case 'listaCelulaAdministrar':
-            $(callout).addClass('callout-warning');
-            break;
-        case 'listaCelulaGerencia':
-            $(callout).addClass('callout-info');
-            break;
-        case 'listaCelulaPreparar':
-            $(callout).addClass('callout-success');
-            break;
-        case 'listaCelulaContratacao':
-            $(callout).addClass('callout-danger');
-            break;
-    }
-}
-
-/***************************************************\
-| Animação de clicar & arrastar e salvar alterações |
-\***************************************************/
-
-$( '#listaCelulaAdministrar, #listaCelulaGerencia, #listaCelulaPreparar, #listaCelulaContratacao' )
-    .sortable({
-        connectWith: '.connectedSortable',
-        receive: function( event, ui ) {
-            // console.log(ui);
-            // console.log(ui.item);
-            // console.log(ui.item[0].id);
-            // console.log(ui.item[0].parentElement.id);
-            // console.log(ui.item[0].children);
-
-            let matricula = ui.item[0].id;
-            let celula = ui.item[0].parentElement.id;
-            let callout = ui.item[0].children;
-
-            // console.log({matricula, celula, _token});
-
-            $.post('/url', {matricula, celula, _token}, function (){
-                trocarCor(celula, callout);
-
-                Toast.fire({
-                    icon: 'success',
-                    title: 'Alteração salva!'
-                });
-            })
-            .fail(function () {
-                
-                $(ui.sender).sortable('cancel');
-
-                Toast.fire({
-                    icon: 'error',
-                    title: 'Erro: alteração Não efetuada!'
-                });
             });
 
-        }
-    })
-    .disableSelection()
-;
+            // console.log(arrayEmpregados);
+
+            let stringEmpregados = arrayEmpregados.join(' ').trim();
+
+            let lista =
+                `<div class="col-md-3">` +
+                    `<div class="card card-default">` +
+                        `<div class="card-header">` +
+                            `<h3 class="card-title">` +
+                                `<b>Célula ` + item.nomeCelula + `</b>` +
+                                `<br>` +
+                                `Gestor: ` + item.nomeGestorCelula +
+                            `</h3>` +
+                        `</div>` +
+                        `<div class="card-body">` +
+                            `<ul class="connectedSortable list-unstyled">` +
+                            stringEmpregados +
+                            `</ul>` +
+                        `</div>` +
+                    `</div>` +
+                `</div>`
+            ;
+
+            $(lista).appendTo('#equipes');
+
+            $('#eventual' + item.matriculaEventualCelula).show();
+
+        });
+
+        /***************************************************\
+        | Animação de clicar & arrastar e salvar alterações |
+        \***************************************************/
+
+        $( '.connectedSortable' )
+        .sortable({
+            connectWith: '.connectedSortable',
+            receive: function( event, ui ) {
+                // console.log(ui);
+                // console.log(ui.item);
+                // console.log(ui.item[0].id);
+                // console.log(ui.item[0].parentElement.id);
+                // console.log(ui.item[0].children);
+
+                let matricula = ui.item[0].id;
+                let celula = ui.item[0].parentElement.id;
+                let callout = ui.item[0].children;
+
+                // console.log({matricula, celula, _token});
+
+                $.post('/url', {matricula, celula, _token}, function (){
+                    // trocarCor(celula, callout);
+
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Alteração salva!'
+                    });
+                })
+                .fail(function () {
+                    
+                    $(ui.sender).sortable('cancel');
+
+                    Toast.fire({
+                        icon: 'error',
+                        title: 'Erro: alteração Não efetuada!'
+                    });
+                });
+
+            }
+        })
+        .disableSelection()
+        ;
+        
+    });
+
+    
+});
