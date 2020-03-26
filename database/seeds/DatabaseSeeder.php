64854f5a4677f07478a44ed59b8c40d5f33184d0
EDUARDO CHIAKI CHUMAN <eduardo.chuman@caixa.gov.br>
2020-03-26 18:19:13 -0300

    ajuste dos seeds com inclusão de truncate antes de popular as tabelas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
                 EmpregadosTableSeeder::class,
                 DistratoDemandasTableSeeder::class,
                 PerfisAcessoPortalTableSeeder::class,
+                FornecedoresDadosDespachanteSeeder::class,
             ]);
         }
         $this->call([
