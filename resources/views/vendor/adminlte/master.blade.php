0bf818e20447f85f8e0a561116e232cedb848248
EDUARDO CHIAKI CHUMAN <eduardo.chuman@caixa.gov.br>
2020-04-13 17:19:04 -0300

    ajuste do favicon do portal

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -9,7 +9,7 @@
 @yield('title', config('adminlte.title', 'AdminLTE 3'))
 @yield('title_postfix', config('adminlte.title_postfix', ''))</title>
     @if(! config('adminlte.enabled_laravel_mix'))
-    <link rel="shortcut icon" href="img/indice.ico" >
+    <link rel="shortcut icon" href="/img/indice.ico" >
     <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
 
