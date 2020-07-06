ee7b54135621ab67a9e08fda9aa2697f8911baea
Chili42 <rafael.economia@yahoo.com.br>
2020-07-05 23:29:25 -0300

    pequenas alterações em produção

--- resources/views/portal/imoveis/componentes/tabs-dados-imovel.blade.php
+++ resources/views/portal/imoveis/componentes/tabs-dados-imovel.blade.php
@@ -1143,6 +1143,7 @@
                                                     <select name="tipoAtendimento" class="form-control" required>
                                                         <option value="">Selecione</option>
                                                         <option value="ANALISE">ANÁLISE/SUBSIDIO</option>
+                                                        <option value="CAR-CAC">CAR/CAC</option>
                                                         <option value="EMAIL">E-MAIL</option>
                                                         <option value="PRESENCIAL">PRESENCIAL</option>
                                                         <option value="OUVIDORIA">SAC/OUVIDORIA</option>
@@ -1152,7 +1153,7 @@
                                                 </div>
 
                                                 <div class="form-group">
-                                                    <label>Coordenação:</label>
+                                                    <label>Atividade:</label>
                                                     <select name="atividadeAtendimento" class="form-control" required>
                                                         <option value="">Selecione</option>
                                                         <option value="CONTRATACAO">CONTRATAÇÃO</option>
@@ -1230,7 +1231,7 @@
                                     <div class="alert" style="background-color: #fff9c2;" >
                                         <div>
                                             <i class="fas fa-exclamation-triangle"></i> ATENÇÃO:
-                                                Envie a autorização apenas após o pagamento da PP15
+                                                Envie a autorização apenas após o pagamento da PP15 / Boleto
                                         </div>
                                     </div>
                                 </div>
